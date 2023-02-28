664c5cdd124c9ebfe49539f68f84bccd74230a76
MatheusRosignol <matheusrosig5@gmail.com>
2023-02-27 22:12:17 -0300

    Py

--- Matheus.py
+++ Matheus.py
@@ -7,3 +7,11 @@ print (30 == 30)
 print(31 ==30)
 print(type(30+31))
 print(type("Jao"))
+print(int("1")+ 1)
+
+Nome = "Matheus Batista Rosignol"
+Idade = 23
+Peso = 93
+if Idade > 18:
+    print("Você é maioridade!")
+print(Nome, Idade, Peso)
