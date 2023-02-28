1110e9c4ddcbff47ee77f57f2d14d12151f27788
MatheusRosignol <matheusrosig5@gmail.com>
2023-02-27 22:18:24 -0300

    Py

--- Matheus.py
+++ Matheus.py
@@ -1,17 +1,10 @@
-#My comment
-print("1+1")
-# Aula univale - Boa noite Alunos.
-print(22, 33, 44, sep ="###")
-print("Matheus", 32, sep="-#-#-#-#-#-")
-print (30 == 30)
-print(31 ==30)
-print(type(30+31))
-print(type("Jao"))
-print(int("1")+ 1)
-
 Nome = "Matheus Batista Rosignol"
-Idade = 23
+Idade = 17
 Peso = 93
-if Idade > 18:
+if Idade >= 18:
     print("Você é maioridade!")
+else:
+    print("Você não é maioridade")
 print(Nome, Idade, Peso)
+
+
