5afb7874f6e97d7d9a131a54fb267fc42e6e7ea7
MatheusRosignol <matheusrosig5@gmail.com>
2023-02-27 21:50:01 -0300

    Py

--- Matheus.py
+++ Matheus.py
@@ -1,3 +1,7 @@
 #My comment
 print("1+1")
-print(22, 33, 44)
+# Aula univale - Boa noite Alunos.
+print(22, 33, 44, sep ="###")
+print("Matheus", 32, sep="-#-#-#-#-#-")
+print (30 == 30)
+print(31 ==30)
